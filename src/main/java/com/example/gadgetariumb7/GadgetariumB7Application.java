9c6f69f966d0cfad4e3f8866c57d4a60ca38bbdc
admin <muhammedbatyrov1015@gmail.com>
2023-01-03 17:01:42 +0600

    application and html commit done

--- src/main/java/com/example/gadgetariumb7/GadgetariumB7Application.java
+++ src/main/java/com/example/gadgetariumb7/GadgetariumB7Application.java
@@ -2,12 +2,19 @@ package com.example.gadgetariumb7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class GadgetariumB7Application {
 
     public static void main(String[] args) {
         SpringApplication.run(GadgetariumB7Application.class, args);
     }
 
+    @GetMapping("/")
+    public String greetings() {
+        return "introduction";
+    }
 }
