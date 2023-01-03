b205e413eb343b1a61e096a96b9fe02a455f9027
admin <muhammedbatyrov1015@gmail.com>
2023-01-03 16:53:43 +0600

    files

---
+++ src/main/java/com/example/gadgetariumb7/GadgetariumB7Application.java
@@ -0,0 +1,13 @@
+package com.example.gadgetariumb7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetariumB7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadgetariumB7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gadgetariumb7/GadgetariumB7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gadgetariumb7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetariumB7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
