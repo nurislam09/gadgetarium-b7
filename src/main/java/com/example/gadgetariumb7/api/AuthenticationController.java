4a101045432fa5dc0e6e9682225b5b717525100f
User <mirgulabdinazarova@gmail.com>
2023-01-18 13:48:50 +0600

    update authentication controller

--- src/main/java/com/example/gadgetariumb7/api/AuthenticationController.java
+++ src/main/java/com/example/gadgetariumb7/api/AuthenticationController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import com.google.firebase.auth.FirebaseAuthException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
