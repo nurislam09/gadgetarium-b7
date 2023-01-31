1cf4a0168c97ac826bdaef375ec69cf171005610
User <mirgulabdinazarova@gmail.com>
2023-01-31 11:37:09 +0600

    fix count

--- src/main/java/com/example/gadgetariumb7/db/repository/OrderRepository.java
+++ src/main/java/com/example/gadgetariumb7/db/repository/OrderRepository.java
@@ -15,36 +15,37 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 
-     @Query("select new com.example.gadgetariumb7.dto.response.OrderResponse(o.id," +
-             "concat(o.firstName,' ', o.lastName ), "+
-             "o.orderNumber," +
-             "o.dateOfOrder," +
-             "o.orderType," +
-             "o.orderStatus) from Order o where o.orderStatus = :orderStatus")
-     List<OrderResponse> findAllOrdersByStatus(OrderStatus orderStatus,Pageable pageable);
-
-
-
-
-    @Query ("select  count (o) from  Order o where  o.orderStatus = :orderStatus")
-    Long countByOrderStatus (OrderStatus orderStatus);
-
-    @Query ("select count (o) from  Order  o")
-    Long getCountOfOrders();
-
-
-    @Query("select new com.example.gadgetariumb7.dto.response.OrderResponse(o.id," +
-            "concat(o.firstName,' ', o.lastName ), "+
+    @Query("select new com.example.gadgetariumb7.dto.response.OrderResponse" +
+            "(o.id," +
+            "concat(o.firstName,' ', o.lastName ), " +
             "o.orderNumber," +
             "o.dateOfOrder," +
             "o.countOfProduct,"+
             "o.totalSum,"+
             "o.orderType," +
+            "o.orderStatus) from Order o where o.orderStatus = :orderStatus")
+    List<OrderResponse> findAllOrdersByStatus(OrderStatus orderStatus, Pageable pageable);
+
+
+    @Query("select  count (o) from  Order o where  o.orderStatus = :orderStatus")
+    Long countByOrderStatus(OrderStatus orderStatus);
+
+    @Query("select count (o) from  Order  o")
+    Long getCountOfOrders();
+
+//    @Query("select  sum (p.orderCount)from Order o join Product  p on o.id=p.order_id")
+//    int orderCount(Long id);
+
+    @Query("select new com.example.gadgetariumb7.dto.response.OrderResponse(o.id," +
+            "concat(o.firstName,' ', o.lastName ), " +
+            "o.orderNumber," +
+            "o.dateOfOrder," +
+            "o.countOfProduct," +
+            "o.totalSum," +
+            "o.orderType," +
             "o.orderStatus) from Order o where  concat( concat(o.firstName,' ', o.lastName ),' ', o.orderNumber," +
             " ' ', o.orderType,' ', o.orderStatus) like %:keyWord%")
     List<OrderResponse> search(@Param("keyWord") String keyWord, Pageable pageable);
-
-
 
 
 }
--- src/main/java/com/example/gadgetariumb7/db/service/serviceImpl/OrderServiceImpl.java
+++ src/main/java/com/example/gadgetariumb7/db/service/serviceImpl/OrderServiceImpl.java
@@ -4,8 +4,13 @@ package com.example.gadgetariumb7.db.service.serviceImpl;
 import com.example.gadgetariumb7.db.entity.Order;
 import com.example.gadgetariumb7.db.entity.Product;
 import com.example.gadgetariumb7.db.entity.User;
+import com.example.gadgetariumb7.db.enums.DeliveryStatus;
 import com.example.gadgetariumb7.db.enums.OrderStatus;
+import com.example.gadgetariumb7.db.enums.OrderType;
+import com.example.gadgetariumb7.db.enums.Payment;
 import com.example.gadgetariumb7.db.repository.OrderRepository;
+import com.example.gadgetariumb7.db.repository.ProductRepository;
+import com.example.gadgetariumb7.db.repository.UserRepository;
 import com.example.gadgetariumb7.db.service.OrderService;
 import com.example.gadgetariumb7.dto.response.OrderResponse;
 import com.example.gadgetariumb7.dto.response.SimpleResponse;
@@ -14,21 +19,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+    private final UserRepository userRepository;
     private final OrderRepository orderRepository;
 
-   @Override
+    private final ProductRepository productRepository;
+    @Override
     public List<OrderResponse> findAllOrdersByStatus(OrderStatus orderStatus, int page, int size) {
         List<OrderResponse> orderResponses = orderRepository.findAllOrdersByStatus(orderStatus, PageRequest.of(page - 1, size));
         for (OrderResponse orderResponse : orderResponses) {
             User user = orderRepository.getById(orderResponse.getId()).getUser();
-
-            calculateOrderInfo(orderResponse, user);
+            calculateOrderInfo(orderResponse,user);
         }
 
         return orderResponses;
@@ -37,31 +46,31 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderResponse> searchOrders(String keyWord, int page, int size) {
-        List<OrderResponse >orderResponses =  orderRepository.search(keyWord, PageRequest.of(page - 1, size));
-       for (OrderResponse orderResponse : orderResponses) {
-            User user = orderRepository.getById(orderResponse.getId()).getUser();
+        List<OrderResponse> orderResponses = orderRepository.search(keyWord, PageRequest.of(page - 1, size));
+        for (OrderResponse orderResponse : orderResponses) {
+            User user = orderRepository.findById(orderResponse.getId()).orElseThrow(() -> new NotFoundException("Order not found")).getUser();
 
-           calculateOrderInfo(orderResponse,user);
-       }
+            calculateOrderInfo(orderResponse, user);
+        }
         return orderResponses;
 
     }
 
 
-   @Override
+    @Override
     public Long countByOrderStatus(OrderStatus orderStatus) {
         return orderRepository.countByOrderStatus(orderStatus);
     }
 
     public SimpleResponse deleteOrderById(Long id) {
-        Order order = orderRepository.findById(id).orElseThrow(() -> new NotFoundException("order not found"));
+        Order order = orderRepository.findById(id).orElseThrow(() -> new NotFoundException("Order not found"));
         orderRepository.delete(order);
         return new SimpleResponse("Order successfully deleted!", "ok");
     }
 
     @Override
     public Long getCountOfOrders() {
-          return orderRepository.getCountOfOrders();
+        return orderRepository.getCountOfOrders();
     }
 
     public void calculateOrderInfo(OrderResponse orderResponse, User user) {
@@ -84,6 +93,49 @@ public class OrderServiceImpl implements OrderService {
         }
         orderResponse.setCountOfProduct(orderCount);
     }
+
+//        @PostConstruct
+//        private void initOrders () {
+//
+//            User user = userRepository.findById(1L).get();
+//
+//
+//            Product product1 = new Product("Iphone", 5000, 5);
+//            Product product2 = new Product("Sumsung", 8000, 10);
+//            Product product3 = new Product("MI", 40000, 15);
+//            Product product4 = new Product("Apple", 30000, 30);
+//
+////            Product product1 = productRepository.findById(1L).orElseThrow(()->new NotFoundException("Product not found"));
+////            Product product2 = productRepository.findById(2L).orElseThrow(()->new NotFoundException("Product not found"));
+////
+//
+//            user.setBasketList(Arrays.asList(product1, product2));
+//
+//            Order order = new Order();
+//            order.setFirstName(user.getFirstName());
+//            order.setLastName(user.getLastName());
+//            order.setDateOfOrder(LocalDateTime.now());
+//            order.setOrderStatus(OrderStatus.CANCEL);
+//            order.setOrderNumber(1);
+//            order.setEmail(user.getEmail());
+//            order.setAddress("Chyi");
+//            order.setOrderType(OrderType.DELIVERY);
+//            order.setDeliveryStatus(DeliveryStatus.WAITING);
+//            order.setPayment(Payment.PAYMENT_WITH_CARD);
+//            order.setPhoneNumber(user.getPhoneNumber());
+////            order.setProducts(Arrays.asList(product1, product2));
+//
+//            order.setUser(user);
+//            user.setOrders(Arrays.asList(order));
+//
+//            order.setProducts(user.getBasketList());
+//            user.getBasketList().forEach(x -> x.setOrders(Arrays.asList(order)));
+//
+//            userRepository.save(user);
+//            orderRepository.save(order);
+//            productRepository.save(product1);
+//            productRepository.save(product2);
+//        }
 
 }
 
