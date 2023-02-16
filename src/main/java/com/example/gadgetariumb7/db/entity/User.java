ef8f662fbfc87de1a15000032cafd5afe1784a65
SyimykRavshanbekov <programmist00707@gmail.com>
2023-02-16 14:26:34 +0600

    change relationship

--- src/main/java/com/example/gadgetariumb7/db/entity/User.java
+++ src/main/java/com/example/gadgetariumb7/db/entity/User.java
@@ -48,7 +48,7 @@ public class User implements UserDetails {
     @OneToMany(cascade = {MERGE, DETACH, REFRESH})
     private List<Product> compareProductsList;
 
-    @OneToMany(cascade = {MERGE, DETACH, REFRESH})
+    @ManyToMany(cascade = {MERGE, DETACH, REFRESH})
     private List<Product> orderHistoryList;
 
     @OneToMany(cascade = {MERGE, DETACH, REFRESH})
