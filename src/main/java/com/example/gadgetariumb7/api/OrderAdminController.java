6aac15c63b3e2d3f28301b85742b70cbbe9d21c7
User <mirgulabdinazarova@gmail.com>
2023-02-24 17:08:55 +0600

    commit6

--- src/main/java/com/example/gadgetariumb7/api/OrderAdminController.java
+++ src/main/java/com/example/gadgetariumb7/api/OrderAdminController.java
@@ -2,6 +2,7 @@ package com.example.gadgetariumb7.api;
 
 import com.example.gadgetariumb7.db.enums.OrderStatus;
 import com.example.gadgetariumb7.db.service.OrderService;
+import com.example.gadgetariumb7.dto.response.OrderInfoResponse;
 import com.example.gadgetariumb7.dto.response.OrderPaymentResponse;
 import com.example.gadgetariumb7.dto.response.PaginationOrderResponse;
 import com.example.gadgetariumb7.dto.response.SimpleResponse;
--- src/main/java/com/example/gadgetariumb7/dto/response/OrderInfoResponse.java
+++ src/main/java/com/example/gadgetariumb7/dto/response/OrderInfoResponse.java
@@ -1,4 +1,17 @@
 package com.example.gadgetariumb7.dto.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderInfoResponse {
+    private int orderNumber;
+
+    private String phoneNumber;
+
+    private String address;
+
 }
