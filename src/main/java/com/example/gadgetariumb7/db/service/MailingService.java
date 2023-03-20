ce03196de4569a38a5f45fce62ad06d842328139
Nursultan Zhumataev <zhumataev.03@gmail.com>
2023-03-20 21:23:14 +0600

    done the send mail to customer to notify about the status his order. Also show the order number.

--- src/main/java/com/example/gadgetariumb7/db/service/MailingService.java
+++ src/main/java/com/example/gadgetariumb7/db/service/MailingService.java
@@ -1,6 +1,7 @@
 package com.example.gadgetariumb7.db.service;
 
 import com.example.gadgetariumb7.db.entity.Mailing;
+import com.example.gadgetariumb7.db.entity.Order;
 import com.example.gadgetariumb7.dto.request.MailingRequest;
 import com.example.gadgetariumb7.dto.response.SimpleResponse;
 
@@ -12,4 +13,6 @@ public interface MailingService {
 
     void sendEmail(String email, Mailing mailing);
 
-}
+    void sendEmail(String email, Order order);
+
+    }
