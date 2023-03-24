5f1c88edab228695b6d2424b7ec66b1ff4b530da
Nursultan Zhumataev <zhumataev.03@gmail.com>
2023-03-24 17:26:35 +0600

    add the feature to notify the subscriber immediately about his subscription

--- src/main/java/com/example/gadgetariumb7/dto/request/SubscriptionRequest.java
+++ src/main/java/com/example/gadgetariumb7/dto/request/SubscriptionRequest.java
@@ -5,10 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SubscriptionRequest {
+    @Email(message = "Email should be valid")
+    @NotNull(message = "Email should be not null")
+    @NotBlank(message = "Email should not be empty")
     String email;
 }
